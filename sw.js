11725d4464a7a42144cb1b957e0875208c62086b
davidnikiema799-gif <davidnikiema799@gmail.com>
2026-07-02 19:52:33 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'pretpro-v3';
+const FILES = ['./index.html', './manifest.json', './sw.js', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
